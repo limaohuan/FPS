d5a56917e232c7688b27b47c4742bf66ce37a4a4
limaohuan <110756417+limaohuan@users.noreply.github.com>
2025-02-06 17:09:31 +0800

    Add files via upload

---
+++ Class5/Source/FPS_Project/FPSHUD.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSHUD.h"
+#include "Engine/Canvas.h"
+#include "UObject/ConstructorHelpers.h"
+
+AFPSHUD::AFPSHUD()
+{
+	// 构造器中的逻辑
+}
+
+void AFPSHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+
+}
+
+
+
---
+++ Class5/Source/FPS_Project/FPS_ProjectWeaponComponent.cpp
@@ -0,0 +1,135 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "FPS_ProjectWeaponComponent.h"
+#include "FPS_ProjectCharacter.h"
+#include "FPS_ProjectProjectile.h"
+#include "GameFramework/PlayerController.h"
+#include "Camera/PlayerCameraManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Animation/AnimInstance.h"
+#include "Engine/LocalPlayer.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UFPS_ProjectWeaponComponent::UFPS_ProjectWeaponComponent()
+{
+	// Default offset from the character location for projectiles to spawn
+	MuzzleOffset = FVector(100.0f, 0.0f, 10.0f);
+
+	// 初始弹药数为15
+	BulletNum = 15;
+	
+}
+
+
+void UFPS_ProjectWeaponComponent::Fire()
+{
+	if (Character == nullptr || Character->GetController() == nullptr || !BulletNum)
+	{
+		return;
+	}
+
+	// Try and fire a projectile
+	if (ProjectileClass != nullptr)
+	{
+		UWorld* const World = GetWorld();
+		if (World != nullptr)
+		{
+			APlayerController* PlayerController = Cast<APlayerController>(Character->GetController());
+			const FRotator SpawnRotation = PlayerController->PlayerCameraManager->GetCameraRotation();
+			// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+			const FVector SpawnLocation = GetOwner()->GetActorLocation() + SpawnRotation.RotateVector(MuzzleOffset);
+	
+			//Set Spawn Collision Handling Override
+			FActorSpawnParameters ActorSpawnParams;
+			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+	
+			// Spawn the projectile at the muzzle
+			AFPS_ProjectProjectile* Bullet = World->SpawnActor<AFPS_ProjectProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+
+			// 传回当前的主角
+			Bullet->SetShooterCharacter(Character);
+			
+			// 子弹数减一
+			BulletNum--;
+
+			// 更新角色弹药数
+			Character->SetBulletNum(BulletNum);
+		}
+	}
+	
+	// Try and play the sound if specified
+	if (FireSound != nullptr)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, FireSound, Character->GetActorLocation());
+	}
+	
+	// Try and play a firing animation if specified
+	if (FireAnimation != nullptr)
+	{
+		// Get the animation object for the arms mesh
+		UAnimInstance* AnimInstance = Character->GetMesh1P()->GetAnimInstance();
+		if (AnimInstance != nullptr)
+		{
+			AnimInstance->Montage_Play(FireAnimation, 1.f);
+		}
+	}
+}
+
+bool UFPS_ProjectWeaponComponent::AttachWeapon(AFPS_ProjectCharacter* TargetCharacter)
+{
+	Character = TargetCharacter;
+
+	// Check that the character is valid, and has no weapon component yet
+	if (Character == nullptr || Character->GetInstanceComponents().FindItemByClass<UFPS_ProjectWeaponComponent>())
+	{
+		return false;
+	}
+
+	// Attach the weapon to the First Person Character
+	FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, true);
+	AttachToComponent(Character->GetMesh1P(), AttachmentRules, FName(TEXT("GripPoint")));
+
+	// Set up action bindings
+	if (APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// Set the priority of the mapping to 1, so that it overrides the Jump action with the Fire action when using touch input
+			Subsystem->AddMappingContext(FireMappingContext, 1);
+		}
+
+		if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		{
+			// Fire
+			EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &UFPS_ProjectWeaponComponent::Fire);
+		}
+	}
+
+	// 更新角色弹药数
+	Character->SetBulletNum(BulletNum);
+
+	return true;
+}
+
+void UFPS_ProjectWeaponComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	// ensure we have a character owner
+	if (Character != nullptr)
+	{
+		// remove the input mapping context from the Player Controller
+		if (APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+			{
+				Subsystem->RemoveMappingContext(FireMappingContext);
+			}
+		}
+	}
+
+	// maintain the EndPlay call chain
+	Super::EndPlay(EndPlayReason);
+}
